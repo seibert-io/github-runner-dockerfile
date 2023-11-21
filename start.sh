6c62f810f57333e921f1fbbdd52599f944ea747a
David Seibert <33252140+seibert-io@users.noreply.github.com>
2023-11-21 15:14:50 +0100

    fix: typo

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 #$ACCESS_TOKEN
 #$REGISTRATION_TOKEN
 
-if [ -z ${var+x} ]; then 
+if [ -z ${REGISTRATION_TOKEN+x} ]; then 
     echo "Fetching REGISTRATION_TOKEN from Github..."; 
     REGISTRATION_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
